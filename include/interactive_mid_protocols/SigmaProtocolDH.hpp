09f8c32fdb9fa7bca2646b9a6d5b6e43a8536987
moriya <moriyaw@gmail.com>
2016-03-31 14:09:48 +0300

    Added sigma Protocol DH.

---
+++ include/interactive_mid_protocols/SigmaProtocolDH.hpp
@@ -0,0 +1,360 @@
+#pragma once
+#include "SigmaProtocol.hpp"
+#include "../primitives/Dlog.hpp"
+
+/**
+* Checks the validity of the given soundness parameter.
+* @return true if the soundness parameter is valid; false, otherwise.
+*/
+bool checkSoundnessParam(DlogGroup* dlog, int t);
+/**
+* Checks if the given challenge length is equal to the soundness parameter.
+* @return true if the challenge length is t; false, otherwise.
+*/
+bool checkChallengeLength(int t, int size);
+
+/**
+* Concrete implementation of SigmaProtocol input, used by the SigmaDH verifier and simulator.<p>
+* In SigmaProtocolDH, the common input contains three GroupElements - h, u, v.
+*
+* @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+*
+*/
+class SigmaDHCommonInput : public SigmaCommonInput {
+public:
+	/**
+	* Sets the common input of this sigma protocol.
+	* @param h
+	* @param u
+	* @param v
+	*/
+	SigmaDHCommonInput(shared_ptr<GroupElement> h, shared_ptr<GroupElement> u, shared_ptr<GroupElement> v) {
+		this->h = h;
+		this->u = u;
+		this->v = v;
+	}
+
+	shared_ptr<GroupElement> getH() { return h; }
+
+	shared_ptr<GroupElement> getU() { return u; }
+
+	shared_ptr<GroupElement> getV() { return v; }
+
+private:
+	shared_ptr<GroupElement> h;
+	shared_ptr<GroupElement> u;
+	shared_ptr<GroupElement> v;
+
+};
+
+/**
+* Concrete implementation of SigmaProtocol input, used by the SigmaDHProver.<p>
+* In SigmaProtocolDH, the prover gets three GroupElements - h, u, v and a BigInteger w such that g^w = u and h^w = v.
+*
+* @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+*
+*/
+class SigmaDHProverInput: public SigmaProverInput {
+public:
+	/**
+	* Sets the prover's input values that satisfy g^w = u and h^w = v.
+	* @param h
+	* @param u
+	* @param v
+	* @param w
+	*/
+	SigmaDHProverInput(shared_ptr<GroupElement> h, shared_ptr<GroupElement> u, shared_ptr<GroupElement> v, biginteger w) {
+		params = make_shared<SigmaDHCommonInput>(h, u, v);
+		this->w = w;
+	}
+	biginteger getW() {	return w; }
+	shared_ptr<SigmaCommonInput> getCommonInput() override { return params; }
+
+private:
+	shared_ptr<SigmaDHCommonInput> params;
+	biginteger w;
+};
+
+/**
+* Concrete implementation SigmaProtocol message. <p>
+* This message contains two GroupElement sendable data and used when the DH prover send the first message to the verifier.
+*
+* @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+*
+*/
+class SigmaDHMsg: public SigmaProtocolMsg {
+
+public:
+	SigmaDHMsg(shared_ptr<GroupElementSendableData> a, shared_ptr<GroupElementSendableData> b) {
+		this->a = a;
+		this->b = b;
+	}
+	shared_ptr<GroupElementSendableData> getA() { return a;	}
+	shared_ptr<GroupElementSendableData> getB() { return b;	}
+	
+	// SerializedNetwork implementation:
+	void initFromString(const string & s) override;
+	string toString() override { return a->toString() + ":" + b->toString(); };
+
+private:
+	shared_ptr<GroupElementSendableData> a;
+	shared_ptr<GroupElementSendableData> b;
+
+};
+
+/**
+* Concrete implementation of SigmaSimulatorOutput, used by SigmaDHSimulator.<p>
+*
+* It contains the a, e, z types used in the above mentioned concrete simulator.
+*
+* @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+*
+*/
+class SigmaDHSimulatorOutput : public SigmaSimulatorOutput {
+
+public:
+	/**
+	* Sets the given messages and challenge.
+	* @param a protocol's first message
+	* @param e protocol's challenge
+	* @param z protocol's second message
+	*/
+	SigmaDHSimulatorOutput(shared_ptr<SigmaDHMsg> a, vector<byte> e, shared_ptr<SigmaBIMsg> z) {
+		this->a = a;
+		this->e = e;
+		this->z = z;
+	}
+
+	/**
+	* Returns the protocol's first message.
+	* @return protocol's first message.
+	*/
+	shared_ptr<SigmaProtocolMsg> getA() override {	return a; }
+
+	/**
+	* Returns the protocol's challenge.
+	* @return protocol's challenge.
+	*/
+	 vector<byte> getE() override { return e; }
+
+	/**
+	* Returns the protocol's second message.
+	* @return protocol's second message.
+	*/
+	 shared_ptr<SigmaProtocolMsg> getZ() override {	return z; }
+
+private:
+	shared_ptr<SigmaDHMsg> a;		//first message
+	vector<byte> e;					//challenge
+	shared_ptr<SigmaBIMsg> z;		//second message
+
+};
+
+/**
+* Concrete implementation of Sigma Simulator.<p>
+* This implementation simulates the case that the prover convince a verifier that the input tuple (g,h,u,v)
+* is a Diffie-Hellman tuple.<p>
+*
+* For more information see Protocol 6.2.4, page 152 of Hazay-Lindell.<p>
+* The pseudo code of this protocol can be found in Protocol 1.2 of pseudo codes document at {@link http://cryptobiu.github.io/scapi/SDK_Pseudocode.pdf}.<p>
+* @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+*
+*/
+class SigmaDHSimulator : public SigmaSimulator {
+	/*
+	This class computes the following calculations:
+	SAMPLE a random z <- Zq
+	COMPUTE a = g^z*u^(-e) and b = h^0z*v^(-e) (where -e here means -e mod q)
+	OUTPUT ((a,b),e,z)
+
+	*/
+public:
+
+	/**
+	* Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+	* @param dlog
+	* @param t Soundness parameter in BITS.
+	* @param random
+	*/
+	SigmaDHSimulator(shared_ptr<DlogGroup> dlog, int t, mt19937 random);
+
+	/**
+	* Returns the soundness parameter for this Sigma protocol.
+	* @return t soundness parameter
+	*/
+	int getSoundnessParam() { return t;	}
+
+	/**
+	* Computes the simulator computation.
+	* @param input MUST be an instance of SigmaDHCommonInput.
+	* @param challenge
+	* @return the output of the computation - (a, e, z).
+	* @throws CheatAttemptException if the received challenge's length is not equal to the soundness parameter.
+	* @throws IllegalArgumentException if the given input is not an instance of SigmaDHCommonInput.
+	*/
+	shared_ptr<SigmaSimulatorOutput> simulate(SigmaCommonInput* input, vector<byte> challenge)  override;
+
+	/**
+	* Computes the simulator computation.
+	* @param input MUST be an instance of SigmaDHInput.
+	* @return the output of the computation - (a, e, z).
+	* @throws IllegalArgumentException if the given input is not an instance of SigmaDHInput.
+	*/
+	shared_ptr<SigmaSimulatorOutput> simulate(SigmaCommonInput* input) override;
+
+private:
+
+	shared_ptr<DlogGroup> dlog; 		//Underlying DlogGroup.
+	int t;								//Soundness parameter.
+	mt19937 random;
+	biginteger qMinusOne;
+
+};
+
+/**
+* Concrete implementation of Sigma Protocol prover computation. <p>
+* This protocol is used for a prover to convince a verifier that the input tuple (g,h,u,v)
+* is a Diffie-Hellman tuple.<p>
+*
+* For more information see Protocol 6.2.4, page 152 of Hazay-Lindell.<p>
+* The pseudo code of this protocol can be found in Protocol 1.2 of pseudo codes document at {@link http://cryptobiu.github.io/scapi/SDK_Pseudocode.pdf}.
+*
+* @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+*
+*/
+class SigmaDHProverComputation : public SigmaProverComputation, public DlogBasedSigma {
+
+public:
+	/*
+	This class computes the following calculations:
+	SAMPLE a random r in Zq
+	COMPUTE a = g^r and b = h^r
+	COMPUTE z = r + ew mod q
+	*/
+
+	/**
+	* Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+	* @param dlog
+	* @param t Soundness parameter in BITS.
+	* @param random
+	* @throws IllegalArgumentException if soundness parameter is invalid.
+	*/
+	SigmaDHProverComputation(shared_ptr<DlogGroup> dlog, int t, mt19937 random);
+
+	/**
+	* Returns the soundness parameter for this Sigma protocol.
+	* @return t soundness parameter
+	*/
+	int getSoundnessParam() override { return t; }
+
+	/**
+	* Computes the first message of the protocol.<p>
+	* "SAMPLE a random r in Zq<p>
+	* COMPUTE a = g^r and b = h^r".
+	* @param input MUST be an instance of SigmaDHProverInput.
+	* @return the computed message
+	* @throws IllegalArgumentException if input is not an instance of SigmaDHProverInput.
+	*/
+	shared_ptr<SigmaProtocolMsg> computeFirstMsg(shared_ptr<SigmaProverInput> input) override;
+
+	/**
+	* Computes the second message of the protocol.<p>
+	* "COMPUTE z = (r + ew) mod q".
+	* @param challenge
+	* @return the computed message.
+	* @throws CheatAttemptException if the length of the received challenge is not equal to the soundness parameter.
+	*/
+	shared_ptr<SigmaProtocolMsg> computeSecondMsg(vector<byte> challenge) override;
+
+	/**
+	* Returns the simulator that matches this sigma protocol prover.
+	* @return SigmaDHSimulator
+	*/
+	shared_ptr<SigmaSimulator> getSimulator() override {
+		auto res = make_shared<SigmaDHSimulator>(dlog, t, random);
+		return res;
+	}
+
+private:
+	shared_ptr<DlogGroup> dlog;				// Underlying DlogGroup.
+	int t; 									// soundness parameter in BITS.
+	mt19937 random;							//source of randomness to use.
+	shared_ptr<SigmaDHProverInput> input;	// Contains h, u, v and w. 
+	biginteger r;							// The value chosen in the protocol.
+	biginteger qMinusOne;
+};
+
+
+/**
+* Concrete implementation of Sigma Protocol verifier computation. <p>
+* This protocol is used for a prover to convince a verifier that the input tuple (g,h,u,v)
+* is a Diffie-Hellman tuple.<p>
+*
+* For more information see Protocol 6.2.4, page 152 of Hazay-Lindell.<p>
+* The pseudo code of this protocol can be found in Protocol 1.2 of pseudo codes document at {@link http://cryptobiu.github.io/scapi/SDK_Pseudocode.pdf}.<p>
+* @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+*
+*/
+class SigmaDHVerifierComputation : public SigmaVerifierComputation, public DlogBasedSigma {
+public:
+	/*
+	This class computes the following calculations:
+	SAMPLE a random challenge  e <- {0, 1}^t
+	ACC IFF VALID_PARAMS(G,q,g) = TRUE AND h in G AND g^z = au^e  AND h^z = bv^e
+	*/
+
+	/**
+	* Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+	* @param dlog
+	* @param t Soundness parameter in BITS.
+	* @param random
+	* @throws InvalidDlogGroupException if the given dlog is invalid.
+	* @throws IllegalArgumentException if soundness parameter is invalid.
+	*/
+	SigmaDHVerifierComputation(shared_ptr<DlogGroup> dlog, int t, mt19937 random);
+
+	/**
+	* Returns the soundness parameter for this Sigma protocol.
+	* @return t soundness parameter
+	*/
+	int getSoundnessParam() override { return t;	}
+
+	/**
+	* Samples the challenge of the protocol.<P>
+	* 	"SAMPLE a random challenge e<-{0,1}^t".
+	*/
+	void sampleChallenge() override; 
+
+	/**
+	* Sets the given challenge.
+	* @param challenge
+	*/
+	void setChallenge(vector<byte> challenge) override {
+		e = challenge;
+	}
+
+	/**
+	* Returns the sampled challenge.
+	* @return the challenge.
+	*/
+	vector<byte> getChallenge() override { return e; }
+
+	/**
+	* Computers the protocol's verification.<p>
+	* Computes the following line from the protocol:<p>
+	* 	"ACC IFF VALID_PARAMS(G,q,g) = TRUE AND h in G AND g^z = au^e  AND h^z = bv^e".   <p>
+	* @param input MUST be an instance of SigmaDHCommonInput.
+	* @param z second message from prover
+	* @return true if the proof has been verified; false, otherwise.
+	* @throws IllegalArgumentException if input is not an instance of SigmaDHCommonInput.
+	* @throws IllegalArgumentException if the first message of the prover is not an instance of SigmaDHMsg
+	* @throws IllegalArgumentException if the second message of the prover is not an instance of SigmaBIMsg
+	*/
+	bool verify(SigmaCommonInput* input, SigmaProtocolMsg* a, SigmaProtocolMsg* z) override;
+
+private:
+		shared_ptr<DlogGroup> dlog;			// Underlying DlogGroup.
+		int t; 								//Soundness parameter in BITS.
+		vector<byte> e;						// The challenge.
+		mt19937 random;
+};
---
+++ src/interactive_mid_protocols/SigmaProtocolDH.cpp
@@ -0,0 +1,288 @@
+#include "../../include/interactive_mid_protocols/SigmaProtocolDH.hpp"
+
+/**
+* Checks if the given challenge length is equal to the soundness parameter.
+* @return true if the challenge length is t; false, otherwise.
+*/
+bool checkChallengeLength(int t, int size) { return ((size == (t / 8)) ? true : false); }
+
+void SigmaDHMsg::initFromString(const string & s) {
+	auto str_vec = explode(s, ':');
+	assert(str_vec.size() == 2);
+	a->initFromString(str_vec[0]);
+	b->initFromString(str_vec[1]);
+}
+
+/********************************************/
+/*       Sigma DH Simulator                 */
+/********************************************/
+
+/**
+* Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+* @param dlog
+* @param t Soundness parameter in BITS.
+* @param random
+*/
+SigmaDHSimulator::SigmaDHSimulator(shared_ptr<DlogGroup> dlog, int t, mt19937 random) {
+	//Sets the parameters.
+	this->dlog = dlog;
+	this->t = t;
+
+	//Check the soundness validity.
+	if (!checkSoundnessParam(dlog.get(), t)) {
+		throw invalid_argument("soundness parameter t does not satisfy 2^t<q");
+	}
+
+	this->random = random;
+	qMinusOne = dlog->getOrder() - 1;
+}
+
+/**
+* Computes the simulator computation.
+* @param input MUST be an instance of SigmaDHCommonInput.
+* @param challenge
+* @return the output of the computation - (a, e, z).
+* @throws CheatAttemptException if the received challenge's length is not equal to the soundness parameter.
+* @throws IllegalArgumentException if the given input is not an instance of SigmaDHCommonInput.
+*/
+shared_ptr<SigmaSimulatorOutput> SigmaDHSimulator::simulate(SigmaCommonInput* input, vector<byte> challenge) {
+	//check the challenge validity.
+	if (!checkChallengeLength(t, challenge.size())) {
+		throw CheatAttemptException("the length of the given challenge is differ from the soundness parameter");
+	}
+
+	SigmaDHCommonInput* dhInput = dynamic_cast<SigmaDHCommonInput*>(input);
+	if (dhInput == NULL) {
+		throw invalid_argument("the given input must be an instance of SigmaDHInput");
+	}
+	
+	//Sample a random z <- Zq
+	biginteger z = getRandomInRange(0, qMinusOne, random);
+
+	//Compute a = g^z*u^(-e) (where -e here means -e mod q)
+	auto gToZ = dlog->exponentiate(dlog->getGenerator().get(), z);
+	biginteger e = decodeBigInteger(challenge.data(), challenge.size());
+	biginteger minusE = dlog->getOrder() - e;
+	auto uToE = dlog->exponentiate(dhInput->getU().get(), minusE);
+	auto a = dlog->multiplyGroupElements(gToZ.get(), uToE.get());
+
+	//Compute b = h^z*v^(-e) (where -e here means -e mod q)
+	auto hToZ = dlog->exponentiate(dhInput->getH().get(), z);
+	auto vToE = dlog->exponentiate(dhInput->getV().get(), minusE);
+	auto b = dlog->multiplyGroupElements(hToZ.get(), vToE.get());
+
+	//Output ((a,b),e,z).
+	return make_shared<SigmaDHSimulatorOutput>(make_shared<SigmaDHMsg>(a->generateSendableData(), b->generateSendableData()), challenge, make_shared<SigmaBIMsg>(z));
+
+}
+
+/**
+* Computes the simulator computation.
+* @param input MUST be an instance of SigmaDHInput.
+* @return the output of the computation - (a, e, z).
+* @throws IllegalArgumentException if the given input is not an instance of SigmaDHInput.
+*/
+shared_ptr<SigmaSimulatorOutput> SigmaDHSimulator::simulate(SigmaCommonInput* input) {
+	//Create a new byte array of size t/8, to get the required byte size and fill it with random values.
+	vector<byte> e;
+	gen_random_bytes_vector(e, t / 8, random);
+
+	//Call the other simulate function with the given input and the sampled e.
+	return simulate(input, e);
+}
+
+/********************************************/
+/*       Sigma DH Prover Computation        */
+/********************************************/
+
+/**
+* Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+* @param dlog
+* @param t Soundness parameter in BITS.
+* @param random
+* @throws IllegalArgumentException if soundness parameter is invalid.
+*/
+SigmaDHProverComputation::SigmaDHProverComputation(shared_ptr<DlogGroup> dlog, int t, mt19937 random) {
+
+	//Sets the parameters.
+	this->dlog = dlog;
+	this->t = t;
+
+	//Check the soundness validity.
+	if (!checkSoundnessParam(dlog.get() ,t)) {
+		throw invalid_argument("soundness parameter t does not satisfy 2^t<q");
+	}
+
+	this->random = random;
+	qMinusOne = dlog->getOrder() - 1;
+}
+
+/**
+* Computes the first message of the protocol.<p>
+* "SAMPLE a random r in Zq<p>
+* COMPUTE a = g^r and b = h^r".
+* @param input MUST be an instance of SigmaDHProverInput.
+* @return the computed message
+* @throws IllegalArgumentException if input is not an instance of SigmaDHProverInput.
+*/
+shared_ptr<SigmaProtocolMsg> SigmaDHProverComputation::computeFirstMsg(shared_ptr<SigmaProverInput> input) {
+	this->input = dynamic_pointer_cast<SigmaDHProverInput>(input);
+	if (this->input == NULL) {
+		throw invalid_argument("the given input must be an instance of SigmaDHProverInput");
+	}
+	
+	//Sample random r in Zq
+	r = getRandomInRange(0, qMinusOne, random);
+
+	//Compute a = g^r.
+	auto a = dlog->exponentiate(dlog->getGenerator().get(), r);
+	//Compute b = h^r.
+	shared_ptr<SigmaDHCommonInput> common = dynamic_pointer_cast<SigmaDHCommonInput>(input->getCommonInput());
+	auto b = dlog->exponentiate(common->getH().get(), r);
+	//Create and return SigmaDHMsg with a and b.
+	return make_shared<SigmaDHMsg>(a->generateSendableData(), b->generateSendableData());
+}
+
+/**
+* Computes the second message of the protocol.<p>
+* "COMPUTE z = (r + ew) mod q".
+* @param challenge
+* @return the computed message.
+* @throws CheatAttemptException if the length of the received challenge is not equal to the soundness parameter.
+*/
+shared_ptr<SigmaProtocolMsg> SigmaDHProverComputation::computeSecondMsg(vector<byte> challenge) {
+
+	//check the challenge validity.
+	if (!checkChallengeLength(t, challenge.size())) {
+		throw new CheatAttemptException("the length of the given challenge is differ from the soundness parameter");
+	}
+
+	//Compute z = (r+ew) mod q
+	biginteger q = dlog->getOrder();
+	biginteger e = decodeBigInteger(challenge.data(), challenge.size());
+	biginteger ew = (e * input->getW()) % q;
+	biginteger z = (r + ew) % q;
+
+	r = 0; //reset the random value r
+
+	//Create and return SigmaBIMsg with z.
+	return make_shared<SigmaBIMsg>(z);
+}
+
+/**
+* Checks the validity of the given soundness parameter.
+* @return true if the soundness parameter is valid; false, otherwise.
+*/
+bool checkSoundnessParam(DlogGroup* dlog, int t) {
+	//If soundness parameter does not satisfy 2^t<q, return false.
+	biginteger soundness = mp::pow(biginteger(2), t);
+	biginteger q = dlog->getOrder();
+	return (soundness < q);
+}
+
+/********************************************/
+/*       Sigma DH Verifier Computation      */
+/********************************************/
+
+/**
+* Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+* @param dlog
+* @param t Soundness parameter in BITS.
+* @param random
+* @throws InvalidDlogGroupException if the given dlog is invalid.
+* @throws IllegalArgumentException if soundness parameter is invalid.
+*/
+SigmaDHVerifierComputation::SigmaDHVerifierComputation(shared_ptr<DlogGroup> dlog, int t, mt19937 random) {
+
+	if (!dlog->validateGroup())
+		throw InvalidDlogGroupException("invalid dlog");
+
+	//Sets the parameters.
+	this->dlog = dlog;
+	this->t = t;
+
+	//Check the soundness validity.
+	if (!checkSoundnessParam(dlog.get(), t)) {
+		throw invalid_argument("soundness parameter t does not satisfy 2^t<q");
+	}
+
+	this->random = random;
+
+}
+
+/**
+* Samples the challenge of the protocol.<P>
+* 	"SAMPLE a random challenge e<-{0,1}^t".
+*/
+void SigmaDHVerifierComputation::sampleChallenge() {
+	//Create a new byte array of size t/8, to get the required byte size.
+	gen_random_bytes_vector(e, t/8, random);
+}
+
+/**
+* Computers the protocol's verification.<p>
+* Computes the following line from the protocol:<p>
+* 	"ACC IFF VALID_PARAMS(G,q,g) = TRUE AND h in G AND g^z = au^e  AND h^z = bv^e".   <p>
+* @param input MUST be an instance of SigmaDHCommonInput.
+* @param z second message from prover
+* @return true if the proof has been verified; false, otherwise.
+* @throws IllegalArgumentException if input is not an instance of SigmaDHCommonInput.
+* @throws IllegalArgumentException if the first message of the prover is not an instance of SigmaDHMsg
+* @throws IllegalArgumentException if the second message of the prover is not an instance of SigmaBIMsg
+*/
+bool SigmaDHVerifierComputation::verify(SigmaCommonInput* input, SigmaProtocolMsg* a, SigmaProtocolMsg* z) {
+	SigmaDHCommonInput* dhInput = dynamic_cast<SigmaDHCommonInput*>(input);
+	if (dhInput == NULL) {
+		throw invalid_argument("the given input must be an instance of SigmaDHCommonInput");
+	}
+
+	bool verified = true;
+
+	SigmaDHMsg* firstMsg = dynamic_cast<SigmaDHMsg*>(a);
+	SigmaBIMsg* exponent = dynamic_cast<SigmaBIMsg*>(z);
+	//If one of the messages is illegal, throw exception.
+	if (firstMsg == NULL) {
+		throw invalid_argument("first message must be an instance of SigmaDHMsg");
+	}
+	if (exponent == NULL) {
+		throw invalid_argument("second message must be an instance of SigmaBIMsg");
+	}
+
+	//Get the h from the input and verify that it is in the Dlog Group.
+	auto h = dhInput->getH();
+	//If h is not member in the group, set verified to false.
+	verified = verified && dlog->isMember(h.get());
+	
+	//Get the elements of the first message from the prover.
+	auto aElement = dlog->reconstructElement(true, firstMsg->getA().get());
+	auto bElement = dlog->reconstructElement(true, firstMsg->getB().get());
+
+	//Verify that g^z = au^e:
+	//Compute g^z (left size of the equation).
+	auto left = dlog->exponentiate(dlog->getGenerator().get(), exponent->getMsg());
+	//Compute a*u^e (right side of the verify equation).
+	biginteger eBI = decodeBigInteger(e.data(), e.size()); 	// convert e to biginteger.
+	
+	//Calculate u^e.
+	auto uToe = dlog->exponentiate(dhInput->getU().get(), eBI);
+	//Calculate a*h^e.
+	auto right = dlog->multiplyGroupElements(aElement.get(), uToe.get());
+	//If left and right sides of the equation are not equal, set verified to false.
+	verified = verified && (*left == *right);
+	
+	//Verify that h^z = bv^e:
+	//Compute h^z (left size of the equation).
+	left = dlog->exponentiate(h.get(), exponent->getMsg());
+	//Compute b*v^e (right side of the verify equation).
+	//Calculate v^e.
+	auto vToe = dlog->exponentiate(dhInput->getV().get(), eBI);
+	//Calculate b*v^e.
+	right = dlog->multiplyGroupElements(bElement.get(), vToe.get());
+	//If left and right sides of the equation are not equal, set verified to false.
+	verified = verified && (*left == *right);
+	
+	e.clear(); //Delete the random value e for re-use.
+
+	//Return true if all checks returned true; false, otherwise.
+	return verified;
+}
