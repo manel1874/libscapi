37da64d6c023cec18f41111bb75c43c09be89ef3
moriya <moriyaw@gmail.com>
2016-06-09 11:05:18 +0300

    Adding ByteArrayRandomValue

--- include/interactive_mid_protocols/RandomValue.hpp
+++ include/interactive_mid_protocols/RandomValue.hpp
@@ -19,3 +19,16 @@ public:
 	BigIntegerRandomValue(biginteger r) { this->r = r; };
 	biginteger getR() { return r; };
 };
+
+/**
+* Concrete class for byte[] random value.
+* @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
+*
+*/
+class ByteArrayRandomValue : public RandomValue {
+private:
+	vector<byte> r;
+public:
+	ByteArrayRandomValue(vector<byte> r) { this->r = r; }
+	vector<byte> getR() { return r; };
+};
