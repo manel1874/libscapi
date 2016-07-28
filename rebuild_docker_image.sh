47f85f0bb5bc212db822738da89165b3ca9be3e6
Asaf Kleinbort <asaf.kleinbort@gmail.com>
2016-07-28 16:43:48 +0300

    another try on paramatrized Dockerfile

--- rebuild_docker_image.sh
+++ rebuild_docker_image.sh
@@ -11,6 +11,7 @@ docker images -q | xargs --no-run-if-empty docker rmi
 
 echo 'building image' $1 'using dockerfile' $2
 docker build --build-arg tag=$3 --no-cache -t $1 -f $2 .
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 echo 'pushing image to docker hub'
 docker login -u scapicryptobiu -p maliciousyao
