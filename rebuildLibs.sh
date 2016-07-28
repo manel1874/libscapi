0651d5392b316d3fe47e7a35c0e0c1340258aa72
Asaf Kleinbort <asaf.kleinbort@gmail.com>
2016-07-28 17:05:13 +0300

    parametrized from statmend in Dockerfile

--- rebuildLibs.sh
+++ rebuildLibs.sh
@@ -1,6 +1,9 @@
 #$1 - tag for building the docker
 
+base_image='scapicryptobiu/libscapi_base':$1
 image='scapicryptobiu/libscapi_libs:'$1
 dockerfilePath='dockerfiles/DockerfileLibs'
 
+docker pull $base_image
+docker tag libscapi_base $base_image
 ./rebuild_docker_image $image $dockerfilePath $1
