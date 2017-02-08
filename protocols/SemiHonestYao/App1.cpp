fc6c8fd12be224343c1bd107f6f28ed04556fe2f
moriya <moriyaw@gmail.com>
2017-02-08 12:09:52 +0200

    Changed the Yao protocol to create the circuit, communication and ot inside the classes instead of getting that as arguments to the constructors.

--- protocols/SemiHonestYao/App1.cpp
+++ protocols/SemiHonestYao/App1.cpp
@@ -27,168 +27,36 @@
 
 
 #include "YaoParties.hpp"
-#include "../../include/circuits/GarbledCircuitFactory.hpp"
 
-struct YaoConfig {
-	int number_of_iterations;
-	bool print_output;
-	string circuit_type;
-	string circuit_file;
-	string input_file_1;
-	string input_file_2;
-	IpAddress sender_ip;
-	IpAddress receiver_ip;
-	YaoConfig(int n_iterations, bool print, string c_file, string input_file_1,
-		string input_file_2, string sender_ip_str, string rec_ip_str, string circuit_type) {
-		number_of_iterations = n_iterations;
-		print_output = print;
-		circuit_file = c_file;
-		this->input_file_1 = input_file_1;
-		this->input_file_2 = input_file_2;
-		sender_ip = IpAddress::from_string(sender_ip_str);
-		receiver_ip = IpAddress::from_string(rec_ip_str);
-		this->circuit_type = circuit_type;
-	};
-};
-
-
-vector<byte> * readInputAsVector(string input_file) {
-	cout << "reading from file " << input_file << endl;;
-	auto sc = scannerpp::Scanner(new scannerpp::File(input_file));
-	int inputsNumber = sc.nextInt();
-	auto inputVector = new vector<byte>(inputsNumber);
-	for (int i = 0; i < inputsNumber; i++)
-		(*inputVector)[i] = (byte)sc.nextInt();
-	return inputVector;
-}
-
-GarbledBooleanCircuit * create_circuit(YaoConfig yao_config) {
-	return GarbledCircuitFactory::createCircuit(yao_config.circuit_file, 
-		GarbledCircuitFactory::CircuitType::FIXED_KEY_FREE_XOR_HALF_GATES, false);
-}
-
-void execute_party_one(YaoConfig yao_config) {
-	auto start = scapi_now();
-	boost::asio::io_service io_service;
-	SocketPartyData me(yao_config.sender_ip, 1213);
-	SocketPartyData other(yao_config.receiver_ip, 1212);
-	auto channel = make_shared<CommPartyTCPSynced>(io_service, me, other);
-	boost::thread t(boost::bind(&boost::asio::io_service::run, &io_service));
-	print_elapsed_ms(start, "PartyOne: Init");
+void execute_party_one(PartyOne* p1, YaoConfig yao_config) {
 	
-	// create the garbled circuit
-	start = chrono::system_clock::now();
-	//FastGarbledBooleanCircuit * circuit = create_circuit(yao_config);
-
-	GarbledBooleanCircuit* circuit = GarbledCircuitFactory::createCircuit(yao_config.circuit_file, 
-		GarbledCircuitFactory::CircuitType::FIXED_KEY_FREE_XOR_HALF_GATES, false);
-
-	print_elapsed_ms(start, "PartyOne: Creating GarbledBooleanCircuit");
-
-	// create the semi honest OT extension sender
-	SocketPartyData senderParty(yao_config.sender_ip, 7766);
-
-	OTBatchSender * otSender;
-#ifdef _WIN32
-	otSender = new OTSemiHonestExtensionSender(senderParty, 163, 1);
-#else
-	otSender = new OTExtensionBristolSender(senderParty.getPort(), true, channel);
-#endif
-
-	// connect to party two
-	channel->join(500, 5000);
-	
-	// get the inputs of P1 
-	vector<byte>* ungarbledInput = readInputAsVector(yao_config.input_file_1);
-
-	PartyOne * p1;
 	auto all = scapi_now();
-
-	cout << "after reading input";
-	// create Party one with the previous created objects.
-	p1 = new PartyOne(channel.get(), otSender, circuit);
-	for (int i = 0; i < yao_config.number_of_iterations ; i++) {
+	for (int i = 0; i < yao_config.number_of_iterations; i++) {
 		// run Party one
-		p1->setInput(ungarbledInput->data());
 		p1->run();
 	}
 	auto end = std::chrono::system_clock::now();
 	int elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - all).count();
 	cout << "********************* PartyOne ********\nRunning " << yao_config.number_of_iterations <<
-		" iterations took: " << elapsed_ms << " milliseconds" << endl 
+		" iterations took: " << elapsed_ms << " milliseconds" << endl
 		<< "Average time per iteration: " << elapsed_ms / (float)yao_config.number_of_iterations << " milliseconds" << endl;
-	
-	// exit cleanly
-	delete p1;
-	delete circuit;
-	delete otSender;
-	delete ungarbledInput;
 
-	io_service.stop();
-	t.join();
 }
 
-void execute_party_two(YaoConfig yao_config) {
-	// init
-	auto start = scapi_now();
-	boost::asio::io_service io_service;
-	SocketPartyData me(yao_config.receiver_ip, 1212);
-	SocketPartyData other(yao_config.sender_ip, 1213);
-	//SocketPartyData receiverParty(yao_config.receiver_ip, 7766);
-	auto channel = make_shared<CommPartyTCPSynced>(io_service, me, other);
-	boost::thread t(boost::bind(&boost::asio::io_service::run, &io_service));
-	print_elapsed_ms(start, "PartyTwo: Init");
-
-	// create the garbled circuit
-	start = scapi_now();
-	GarbledBooleanCircuit * circuit = create_circuit(yao_config);
-	print_elapsed_ms(start, "PartyTwo: creating FastGarbledBooleanCircuit");
-
-
-
-	// create the OT receiver.
-	start = scapi_now();
-	SocketPartyData senderParty(yao_config.sender_ip, 7766);
-
-	OTBatchReceiver * otReceiver;
-#ifdef _WIN32
-	otReceiver = new OTSemiHonestExtensionReceiver(senderParty, 163, 1);
-#else
-	otReceiver = new OTExtensionBristolReceiver(senderParty.getIpAddress().to_string(), senderParty.getPort(), true, channel);
-#endif
-
-	print_elapsed_ms(start, "PartyTwo: creating OTSemiHonestExtensionReceiver");
-
-	// connect to party one
-	channel->join(500, 5000);
-
-
-	// create Party two with the previous created objec ts			
-	vector<byte> * ungarbledInput = readInputAsVector(yao_config.input_file_2);
-
-	cout << "after reading input";
-
-	PartyTwo * p2;
+void execute_party_two(PartyTwo* p2, YaoConfig yao_config) {
+	
 	auto all = scapi_now();
-	p2 = new PartyTwo(channel.get(), otReceiver, circuit, yao_config.print_output);
-	for (int i = 0; i < yao_config.number_of_iterations ; i++) {
-		// init the P1 yao protocol and run party two of Yao protocol.
-		p2->setInput(&(ungarbledInput->at(0)), ungarbledInput->size());
+	for (int i = 0; i < yao_config.number_of_iterations; i++) {
+		// run party two of Yao protocol.
 		p2->run();
 	}
 	auto end = std::chrono::system_clock::now();
 	int elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - all).count();
 	cout << "********************* PartyTwo ********\nRunning " << yao_config.number_of_iterations <<
 		" iterations took: " << elapsed_ms << " milliseconds" << endl;
-	cout << "Average time per iteration: " << elapsed_ms / (float)yao_config.number_of_iterations 
+	cout << "Average time per iteration: " << elapsed_ms / (float)yao_config.number_of_iterations
 		<< " milliseconds" << endl;
-	delete p2;
-	delete circuit;
-	delete otReceiver;
-	delete ungarbledInput;
 
-	io_service.stop();
-	t.join();
 }
 
 YaoConfig read_yao_config(string config_file) {
@@ -216,18 +84,21 @@ YaoConfig read_yao_config(string config_file) {
 int main(int argc, char* argv[]) {
 
 	int partyNum = atoi(argv[1]);
-
-	YaoConfig yao_config = read_yao_config(argv[2]);
-	if (partyNum == 1)
-		execute_party_one(yao_config);
-	else if (partyNum == 2)
-		execute_party_two(yao_config);
-	else {
+	
+	YaoConfig yao_config(argv[2]);
+	if (partyNum == 1) {
+		// create Party one with the previous created objects.
+		PartyOne p1(yao_config);
+		execute_party_one(&p1, yao_config);
+	}
+	else if (partyNum == 2) {
+		PartyTwo p2(yao_config);
+		execute_party_two(&p2, yao_config);
+	} else {
 		std::cerr << "Usage: libscapi_examples yao <party_number(1|2)> <config_path>" << std::endl;
 		return 1;
 	}
 
-	//system("pause");
 	return 0;
 }
 
