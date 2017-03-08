c68056cbc1e2faa6300a6b4d01d341af70043cc7
Moriya <moriyaw@gmail.com>
2017-03-08 09:54:45 +0200

    Trying to get a better appereance

--- docs/build/html/_sources/index.rst.txt
+++ docs/build/html/_sources/index.rst.txt
@@ -1,10 +1,6 @@
-.. libscapi documentation master file, created by
-   sphinx-quickstart on Wed Feb 22 10:37:57 2017.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to libscapi
 ===================
+
 Libscapi is an open-source c++ library for implementing secure two-party and multiparty computation protocols (SCAPI stands for the "Secure Computation API"). It provides a reliable, efficient, and highly flexible cryptographic infrastructure. SCAPI also has a java version, that can be found at `<https://scapi.readthedocs.io/en/latest/index.html>`_. SCAPI is free and is licensed under an adaptation of the MIT license, you can read more about the license :ref:`here <license>`.
 
 .. toctree::
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'libscapi'
-copyright = u'2017, scapi team'
+copyright = u'2017, libscapi team'
 author = u'cryptobiu'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -84,7 +84,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -129,7 +129,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'libscapi.tex', u'libscapi Documentation',
-     u'scapi team', 'manual'),
+     u'libscapi team', 'manual'),
 ]
 
 
