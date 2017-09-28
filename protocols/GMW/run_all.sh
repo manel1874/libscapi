b203f65e53bf0379c8d1e1ad59578e492ba69550
liorbiu <liork.cryptobiu@gmail.com>
2017-09-28 10:51:31 +0300

    update run_all script & made repititions from outside

--- protocols/GMW/run_all.sh
+++ protocols/GMW/run_all.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 for i in `seq 0 1 2`;
 do
-        ./GMW $i $1 Parties $2 $3 &
+        ./GMW -partyID $i -circuitFileName $1 -partiesFileName $2 -inputfileName AesInputs${i}.txt -numThreads $3 &
         echo "Running $i..."
 done
 
