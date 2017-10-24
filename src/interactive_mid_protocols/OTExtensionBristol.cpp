48ff32078b995f56cd4a5947093b998befb99dc1
liorbiu <liork.cryptobiu@gmail.com>
2017-10-24 15:59:43 +0300

    Hard coded AWS address for dubug onlygit add src/interactive_mid_protocols/OTExtensionBristol.cpp

--- src/interactive_mid_protocols/OTExtensionBristol.cpp
+++ src/interactive_mid_protocols/OTExtensionBristol.cpp
@@ -72,7 +72,7 @@ void OTExtensionBristolBase::shrinkArray(int sourceElementSize, int targetElemen
 OTExtensionBristolSender::OTExtensionBristolSender(int port,bool isSemiHonest, const shared_ptr<CommParty> & channel) {
 
 	//Call the init of the base class. The host name is hard coded to localhost since the sender is the  listener.
-	init("localhost", port, 0, isSemiHonest, channel);
+	init("ec2-54-165-19-113.compute-1.amazonaws.com", port, 0, isSemiHonest, channel);
 }
 
 
