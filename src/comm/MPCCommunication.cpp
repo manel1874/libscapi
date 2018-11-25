9cd5ff85a01af06ee5900d902448335906f50c1e
moriyaw <moriyaw@gmail.com>
2018-11-25 12:10:17 +0200

    Adding new protocol interface that implement some common functionalities like setting the communication, measuring the protocol, etc.

--- src/comm/MPCCommunication.cpp
+++ src/comm/MPCCommunication.cpp
@@ -63,3 +63,60 @@ cout<<"num parties = "<<numParties<<endl;
     return parties;
 
 }
+
+vector<shared_ptr<CommParty>> MPCCommunication::setCommunication(int id, int numParties, string configFile) {
+    cout<<"in communication"<<endl;
+
+    cout<<"num parties = "<<numParties<<endl;
+    cout<<"my id = "<<id<<endl;
+    vector<shared_ptr<CommParty>> parties(numParties);
+
+    //open file
+    ConfigFile cf(configFile);
+
+    string portString, ipString;
+    vector<int> ports(numParties);
+    vector<string> ips(numParties);
+
+    int counter = 0;
+    for (int i = 0; i < numParties; i++) {
+        portString = "party_" + to_string(i) + "_port";
+        ipString = "party_" + to_string(i) + "_ip";
+
+        //get partys IPs and ports data
+        ports[i] = stoi(cf.Value("", portString));
+        ips[i] = cf.Value("", ipString);
+    }
+
+    SocketPartyData me, other;
+
+    for (int i=0; i<numParties; i++){
+
+
+        if (i < id) {// This party will be the receiver in the protocol
+
+            me = SocketPartyData(boost_ip::address::from_string(ips[id]), ports[id] + i);
+            cout<<"my port = "<<ports[id] + i<<endl;
+            other = SocketPartyData(boost_ip::address::from_string(ips[i]), ports[i] + id - 1);
+            cout<<"other port = "<<ports[i] + id - 1<<endl;
+
+        } else if (i>id) {// This party will be the sender in the protocol
+            me = SocketPartyData(boost_ip::address::from_string(ips[id]), ports[id] + i - 1);
+            cout<<"my port = "<<ports[id] + i - 1<<endl;
+            other = SocketPartyData(boost_ip::address::from_string(ips[i]), ports[i] + id);
+            cout<<"other port = "<< ports[i] + id<<endl;
+        }
+
+        if (i!= id) {
+            shared_ptr<CommParty> channel = make_shared<CommPartyTCPSynced>(io_service, me, other);
+
+            // connect to party one
+            channel->join(500, 5000);
+            cout << "channel established" << endl;
+
+            parties[i] = channel;
+        }
+    }
+
+    return parties;
+}
--- src/cryptoInfra/Protocol.cpp
+++ src/cryptoInfra/Protocol.cpp
@@ -60,3 +60,142 @@ CmdParser Protocol::getParser()
 {
     return parser;
 }
+
+MPCProtocol::MPCProtocol(string protocolName, int argc, char* argv[]): Protocol (protocolName, argc, argv){
+
+    vector<string> subTaskNames{"Offline", "Online"};
+    timer = new Measurement(*this, subTaskNames);
+
+
+    partyID = stoi(this->getParser().getValueByKey(arguments, "partyID"));
+    cout<<"ID = "<<partyID<<endl;
+    auto partiesNumber = this->getParser().getValueByKey(arguments, "partiesNumber");
+
+    if (partiesNumber == "NotFound"){
+        numParties = 2;
+    } else {
+        numParties = stoi(this->getParser().getValueByKey(arguments, "partiesNumber"));
+    }
+    cout<<"number of parties = "<<numParties<<endl;
+    auto partiesFile = this->getParser().getValueByKey(arguments, "partiesFile");
+    cout<<"partiesFile = "<<partiesFile<<endl;
+
+    times = stoi(this->getParser().getValueByKey(arguments, "internalIterationsNumber"));
+    parties = comm.setCommunication(partyID, numParties, partiesFile);
+    numThreads = stoi(this->getParser().getValueByKey(arguments, "numThreads"));
+
+    //Calculates the number of threads.
+    if (numParties <= numThreads){
+        this->numThreads = numParties;
+        numPartiesForEachThread = 1;
+    } else{
+        numPartiesForEachThread = (numParties + numThreads - 1)/ numThreads;
+    }
+
+    initTimes();
+
+}
+
+MPCProtocol::~MPCProtocol(){
+    delete timer;
+}
+
+void MPCProtocol::initTimes(){
+    //cout<<"before sending any data"<<endl;
+    byte tmpBytes[20];
+    byte allBytes[20*numParties];
+    roundFunctionSameMsg(tmpBytes, allBytes, 20);
+
+}
+
+void MPCProtocol::run(){
+    auto start = std::chrono::system_clock::now();
+
+    for (currentIteration = 0; currentIteration<times; currentIteration++){
+
+        timer->startSubTask("Offline", currentIteration);
+        runOffline();
+        timer->endSubTask("Offline", currentIteration);
+
+        timer->startSubTask("Online", currentIteration);
+        runOnline();
+        timer->endSubTask("Online", currentIteration);
+
+    }
+
+    auto end = std::chrono::system_clock::now();
+    int elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    cout << "Running " << times <<" iterations took: " << elapsed_ms << " milliseconds" << endl
+         << "Average time per iteration: " << elapsed_ms / (float)times << " milliseconds" << endl;
+
+}
+
+void MPCProtocol::roundFunctionSameMsg(byte* sendData, byte* receiveData, int msgSize){
+    vector<thread> threads(numThreads);
+    //Split the work to threads. Each thread gets some parties to work on.
+    for (int t=0; t<numThreads; t++) {
+        if ((t + 1) * numPartiesForEachThread <= parties.size()) {
+            threads[t] = thread(&MPCProtocol::exchangeDataSameInput, this, sendData, receiveData, t * numPartiesForEachThread, (t + 1) * numPartiesForEachThread, msgSize);
+        } else {
+            threads[t] = thread(&MPCProtocol::exchangeDataSameInput, this, sendData, receiveData, t * numPartiesForEachThread, numParties, msgSize);
+        }
+    }
+    for (int t=0; t<numThreads; t++){
+        threads[t].join();
+    }
+}
+
+
+void MPCProtocol::roundFunctionDiffMsg(byte* sendData, byte* receiveData, int msgSize){
+    vector<thread> threads(numThreads);
+    //Split the work to threads. Each thread gets some parties to work on.
+    for (int t=0; t<numThreads; t++) {
+        if ((t + 1) * numPartiesForEachThread <= parties.size()) {
+            threads[t] = thread(&MPCProtocol::exchangeDataDiffInput, this, sendData, receiveData, t * numPartiesForEachThread, (t + 1) * numPartiesForEachThread, msgSize);
+        } else {
+            threads[t] = thread(&MPCProtocol::exchangeDataDiffInput, this, sendData, receiveData, t * numPartiesForEachThread, numParties, msgSize);
+        }
+    }
+    for (int t=0; t<numThreads; t++){
+        threads[t].join();
+    }
+}
+
+void MPCProtocol::exchangeDataSameInput(byte* sendData, byte* receiveData, int first, int last, int msgSize){
+    for (int j=first; j<last; j++){
+        if (partyID < j) {
+            //send myData to the other party
+            parties[j]->write(sendData, msgSize);
+            //receive the other data from the other party
+            parties[j]->read(receiveData + j*msgSize, msgSize);
+
+        } else if (partyID > j){
+            //receive the other data from the other party
+            parties[j]->read(receiveData + j*msgSize, msgSize);
+            //send myData to the other party
+            parties[j]->write(sendData, msgSize);
+        } else {
+            memcpy(receiveData + j*msgSize, sendData, msgSize);
+        }
+    }
+}
+
+void MPCProtocol::exchangeDataDiffInput(byte* sendData, byte* receiveData, int first, int last, int msgSize){
+    for (int j=first; j<last; j++){
+        if (partyID < j) {
+            //send myData to the other party
+            parties[j]->write(sendData + j*msgSize, msgSize);
+            //receive the other data from the other party
+            parties[j]->read(receiveData + j*msgSize, msgSize);
+
+        } else if (partyID > j){
+            //receive the other data from the other party
+            parties[j]->read(receiveData + j*msgSize, msgSize);
+            //send myData to the other party
+            parties[j]->write(sendData + j*msgSize, msgSize);
+        } else {
+            memcpy(receiveData + j*msgSize, sendData + j*msgSize, msgSize);
+        }
+    }
+}
+
