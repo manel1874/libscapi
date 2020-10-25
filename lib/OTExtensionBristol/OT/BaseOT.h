9056dd707a8944e92c2047a591fa411e11c7ed35
manel1874 <manel1874@gmail.com>
2020-10-25 08:21:01 +0000

    Adding new_receiver_inputs=true

--- lib/OTExtensionBristol/OT/BaseOT.h
+++ lib/OTExtensionBristol/OT/BaseOT.h
@@ -56,7 +56,7 @@ public:
 	int length() { return ot_length; }
 
 	// do the OTs
-	virtual void exec_base(bool new_receiver_inputs);
+	virtual void exec_base(bool new_receiver_inputs=true);
 	// use PRG to get the next ot_length bits
 	void extend_length();
 	void check();
